f3279a721c27a90b457d49a7a8a4e64f14ab32b3
Emanuele Fabbiani <donlelef@gmail.com>
2024-06-29 18:33:40 +0200

    feat: add function to create prompt building pipeline

--- src/movie_embeddings/haystack_pipelines.py
+++ src/movie_embeddings/haystack_pipelines.py
@@ -33,15 +33,24 @@ def build_openai_rag_pipeline(
     embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
     openai_model: str = "gpt-3.5-turbo",
 ) -> Pipeline:
+    pipe = build_prompt_building_pipeline(retriever, prompt_template, embedding_model)
+    pipe.add_component("llm", OpenAIGenerator(model=openai_model))
+    pipe.connect("prompt_builder", "llm")
+    return pipe
+
+
+def build_prompt_building_pipeline(
+    retriever: InMemoryEmbeddingRetriever | QdrantEmbeddingRetriever,
+    prompt_template: str,
+    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
+) -> Pipeline:
     pipe = Pipeline()
     pipe.add_component(
         "embedder", SentenceTransformersTextEmbedder(model=embedding_model)
     )
     pipe.add_component("retriever", retriever)
     pipe.add_component("prompt_builder", PromptBuilder(template=prompt_template))
-    pipe.add_component("llm", OpenAIGenerator(model=openai_model))
 
     pipe.connect("embedder.embedding", "retriever.query_embedding")
     pipe.connect("retriever", "prompt_builder.documents")
-    pipe.connect("prompt_builder", "llm")
     return pipe
