61960c28846b230256b8587aba21c7efa8a8c5b7
Emanuele Fabbiani <donlelef@gmail.com>
2024-06-29 18:01:36 +0200

    feat: add notebook 06-guardrails.ipynb

--- src/movie_embeddings/data.py
+++ src/movie_embeddings/data.py
@@ -1,9 +1,28 @@
 from typing import Literal
 
 import pandas as pd
+from haystack import Document
 
 BASE_URL = "https://github.com/xtreamsrl/genai-for-engineers-class/raw/main/data"
 
 
 def get_dataset(dataset: Literal["movies", "sentences"] = "movies") -> pd.DataFrame:
     return pd.read_parquet(f"{BASE_URL}/{dataset}.parquet")
+
+
+def get_movie_dataset_as_documents(sample: int | None = 1000) -> list[Document]:
+    movie_df = get_dataset("movies")
+    if sample is not None:
+        movie_df = movie_df.sample(sample, random_state=42)
+    documents = [
+        Document(
+            id=movie["id"],
+            content=f'title: {movie["title"]} \noverview: {movie["overview"]}',
+            meta={
+                "title": movie["title"],
+                "release_date": movie["release_date"],
+            },
+        )
+        for movie in movie_df.to_dict(orient="records")
+    ]
+    return documents
---
+++ src/movie_embeddings/haystack_pipelines.py
@@ -0,0 +1,47 @@
+from haystack import Pipeline
+from haystack.components.builders import PromptBuilder
+from haystack.components.embedders import (
+    SentenceTransformersDocumentEmbedder,
+    SentenceTransformersTextEmbedder,
+)
+from haystack.components.generators import OpenAIGenerator
+from haystack.components.retrievers import InMemoryEmbeddingRetriever
+from haystack.components.writers import DocumentWriter
+from haystack.document_stores.types import DocumentStore
+from haystack_integrations.components.retrievers.qdrant import QdrantEmbeddingRetriever
+
+
+def build_indexing_pipline(
+    document_store: DocumentStore,
+    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
+) -> Pipeline:
+    pipe = Pipeline()
+    pipe.add_component(
+        instance=SentenceTransformersDocumentEmbedder(model=embedding_model),
+        name="doc_embedder",
+    )
+    pipe.add_component(
+        instance=DocumentWriter(document_store=document_store), name="doc_writer"
+    )
+    pipe.connect("doc_embedder.documents", "doc_writer.documents")
+    return pipe
+
+
+def build_openai_rag_pipeline(
+    retriever: InMemoryEmbeddingRetriever | QdrantEmbeddingRetriever,
+    prompt_template: str,
+    embedding_model: str = "sentence-transformers/all-MiniLM-L6-v2",
+    openai_model: str = "gpt-3.5-turbo",
+) -> Pipeline:
+    pipe = Pipeline()
+    pipe.add_component(
+        "embedder", SentenceTransformersTextEmbedder(model=embedding_model)
+    )
+    pipe.add_component("retriever", retriever)
+    pipe.add_component("prompt_builder", PromptBuilder(template=prompt_template))
+    pipe.add_component("llm", OpenAIGenerator(model=openai_model))
+
+    pipe.connect("embedder.embedding", "retriever.query_embedding")
+    pipe.connect("retriever", "prompt_builder.documents")
+    pipe.connect("prompt_builder", "llm")
+    return pipe
