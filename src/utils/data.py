7b0571b69778263218bb4deb031a5656eae20312
Emanuele Fabbiani <donlelef@gmail.com>
2025-11-02 14:27:55 +0100

    feat: add notebook 4

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,12 +1,29 @@
 from typing import Literal
 
 import polars as pl
+import requests
+from PIL import Image
 from haystack import Document
 
 
+def _get_data_base_url() -> str:
+    return "https://github.com/xtreamsrl/genai-for-engineers-class/raw/feat/update-notebooks/data"
+
+
+def get_image_dataset() -> list[Image.Image]:
+    n_pictures = 3
+    categories = ["cat", "car"]
+    image_names = [
+        f"{category}{i + 1}.jpeg" for category in categories for i in range(n_pictures)
+    ]
+    urls = [f"{_get_data_base_url()}/images/{image_name}" for image_name in image_names]
+    images = [Image.open(requests.get(url, stream=True).raw) for url in urls]
+    return images
+
+
 def get_dataset(dataset: Literal["movies", "sentences"] = "movies") -> pl.DataFrame:
-    base_url = "https://github.com/xtreamsrl/genai-for-engineers-class/raw/main/data"
-    return pl.read_parquet(f"{base_url}/{dataset}.parquet")
+    url = f"{_get_data_base_url()}/{dataset}.parquet"
+    return pl.read_parquet(url)
 
 
 def get_movie_dataset_as_documents(sample: int | None = 1000) -> list[Document]:
